9e34e6ca8e3381d0cad4f3d2850fa9cb8042fffb
Stanislaw09 <sgardzielewski09@gmail.com>
2021-02-21 11:20:52 +0100

    added firebase to project

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,34 @@
 const express = require('express');
+const firebase=require('firebase')
+require('firebase/firestore')
+
+const firebaseConfig={
+   apiKey: "AIzaSyABxjqUnOPKKPCz-EMgkfF0ESYsHjGFl1E",
+    authDomain: "devian-f33b2.firebaseapp.com",
+    projectId: "devian-f33b2",
+    storageBucket: "devian-f33b2.appspot.com",
+    messagingSenderId: "143462962223",
+    appId: "1:143462962223:web:2a6d74a252c2559c4922da",
+    measurementId: "G-X7HTMZY5XD"
+}
 
 const app = express();
+firebase.initializeApp(firebaseConfig);
+const db=firebase.firestore()
+
+db.collection("sample").add({
+   name: "Ada"
+}).then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+   }).catch((error) => {
+      console.error("Error adding document: ", error);
+   });
+
+db.collection("sample").get().then((querySnapshot) => {
+   querySnapshot.forEach((doc) => {
+         console.log(`${doc.id} => ${doc.data()}`);
+   });
+});
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
