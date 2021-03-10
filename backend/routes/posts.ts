13461a3a7164ca2556561f7a7980e586521cac98
UlaWilk <coyot.szafir@gmail.com>
2021-03-10 17:10:11 +0100

    DELETE/posts and GET/posts/postID with tests

--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -1,45 +1,75 @@
-import express, {Response, Request} from 'express';
-import mongoose from 'mongoose';
+import express, { Response, Request } from "express";
+import mongoose from "mongoose";
 const router = express.Router();
 const Post = require("../models/posts");
 
 router.post("/", (req: Request, res: Response) => {
-    Post.find({title: req.body.title}).exec().then((posts) =>{
-        if(posts.length > 0){
-            return res.status(409).json({ message: "Title of this posts already exists" });
-        }else{
-            Post.find({image: req.body.image}).exec().then((posts) =>{
-                if(posts.length > 0){
-                    return res.status(409).json({message: "That image was already posted"});
-                }else{
-                    const post = new Post({
-                        _id: new mongoose.Types.ObjectId(),
-                        title: req.body.title,
-                        tags: req.body.tags,
-                        image: req.body.image,
-                        author: req.body.author, //autorIdRead() from localstorage will be used here
-                        likes: 0,
-                        comments: []
-                    });
-                    
-                    post.save().then((result) => {
-                        return res.status(201).json(post);
-                    }).catch((err) => {
-                        return res.status(500).json({ error: err });
-                    });
-                }
-            });
-        }
-    })
+  Post.find({ title: req.body.title })
+    .exec()
+    .then(posts => {
+      if (posts.length > 0) {
+        return res
+          .status(409)
+          .json({ message: "Title of this posts already exists" });
+      } else {
+        Post.find({ image: req.body.image })
+          .exec()
+          .then(posts => {
+            if (posts.length > 0) {
+              return res
+                .status(409)
+                .json({ message: "That image was already posted" });
+            } else {
+              const post = new Post({
+                _id: new mongoose.Types.ObjectId(),
+                title: req.body.title,
+                tags: req.body.tags,
+                image: req.body.image,
+                author: req.body.author, //autorIdRead() from localstorage will be used here
+                likes: 0,
+                comments: []
+              });
+
+              post
+                .save()
+                .then(result => {
+                  return res.status(201).json(post);
+                })
+                .catch(err => {
+                  return res.status(500).json({ error: err });
+                });
+            }
+          });
+      }
+    });
 });
 
 router.get("/", async (req, res) => {
-    try {
-      const posts = await Post.find();
-      return res.status(200).json(posts);
-    } catch (err) {
-      return res.status(400).json({ message: err });
-    }
+  try {
+    const posts = await Post.find();
+    return res.status(200).json(posts);
+  } catch (err) {
+    return res.status(400).json({ message: err });
+  }
+});
+
+router.get("/:postId", async (req: Request, res: Response) => {
+  if (mongoose.Types.ObjectId.isValid(req.params.postId)) {
+    const post = await Post.findById(req.params.postId);
+    if (!post)
+      return res.status(404).send("The post with the given ID was not found.");
+    res.send(post);
+  } else res.status(404).send("Invalid ID");
+});
+
+router.delete("/:postId", async (req: Request, res: Response) => {
+  if (mongoose.Types.ObjectId.isValid(req.params.postId)) {
+    const post = await Post.findByIdAndRemove(req.params.postId);
+    if (!post)
+      return res.status(404).send("The post with the given ID was not found.");
+    res.status(200).send(post);
+  } else
+    return res.status(404).send("Invalid ID");
 });
 
 module.exports = router;
--- backend/tests/posts.test.js
+++ backend/tests/posts.test.js
@@ -5,69 +5,116 @@ const mongoose = require("mongoose");
 const Post = require("../build/models/posts");
 
 describe("testing posts", () => {
+  beforeAll(async () => {
+    const url = process.env.DB_CONNECTION;
+    await mongoose.connect(url, {
+      useNewUrlParser: true
+    });
+  });
 
-    beforeAll(async () => {
-        const url = process.env.DB_CONNECTION;
-        await mongoose.connect(url, {
-            useNewUrlParser: true
-        });
+  it("posts endpoint testing", async done => {
+    const resp = await request.get("/posts");
+    expect(resp.status).toBe(200);
+
+    expect(resp.body).toBeDefined();
+    done();
+  });
+
+  it("creating post with existing title", async done => {
+    const resp = await request.post("/posts/").send({
+      title: "dog",
+      tags: "#dog",
+      image: "im",
+      author: 1, //tu będzie wykorzystanie metody localstorage typu autorIdRead() narazie statyczne dane :)
+      likes: 0,
+      comments: []
     });
 
-    it("posts endpoint testing", async (done) => {
-        const resp = await request.get("/posts");
-        expect(resp.status).toBe(200);
+    expect(resp.status).toBe(409);
+    expect(resp.body.message).toBe("Title of this posts already exists");
 
-        expect(resp.body).toBeDefined();
-        done();
+    done();
+  });
+
+  it("adding new post", async done => {
+    const resp = await request.post("/posts/").send({
+      title: "cat",
+      tags: "#dog",
+      image: "cat",
+      author: "2"
     });
 
-    it("creating post with existing title", async (done) => {
-        const resp = await request.post("/posts/").send({
-            title: "dog",
-            tags: "#dog",
-            image: "im",
-            author: 1, //tu będzie wykorzystanie metody localstorage typu autorIdRead() narazie statyczne dane :)
-            likes: 0,
-            comments: []
-        });
+    const post = await Post.findOne({ title: "cat" });
 
-        expect(resp.status).toBe(409);
-        expect(resp.body.message).toBe("Title of this posts already exists");
+    expect(post.id).toBeTruthy();
 
-        done();
+    await Post.findByIdAndDelete(post.id);
+    done();
+  });
+
+  it("getting posts", async done => {
+    const resp = await request.get("/posts");
+
+    expect(resp.status).toBe(200);
+    expect(resp.body).toBeDefined();
+    done();
+  });
+
+  /////// ULA //////
+  it("should response with 404 for getting post with an invalid post ID", async done => {
+    const invalidId = "123invalidID";
+    const resp = await request.get(`/posts/${invalidId}`);
+    expect(resp.status).toBe(404);
+    done();
+  });
+
+  it("should get post with a valid ID", async done => {
+    const resp = await request.get("/posts");
+    console.log;
+    if (resp) {
+      const validId = resp.body[0]._id;
+      const respOne = await request.get(`/posts/${validId}`);
+      expect(respOne.status).toBe(200);
+      expect(respOne.body).toBeDefined();
+    }
+    done();
+  });
+
+  it("should response with 404 for delete a post with invalid ID", async done => {
+    const invalidId = "123invalidID";
+    const resp = await request.delete(`/posts/${invalidId}`);
+    expect(resp.status).toBe(404);
+    done();
+  });
+/*
+//delete without adding post first !!!! DELTE first post in database
+  it("should response with deleted object for delete a post with a valid ID", async done => {
+    const resp = await request.get("/posts");
+    if (resp) {
+      const validId = resp.body[0]._id;
+      const respDel = await request.delete(`/posts/${validId}`);
+      expect(respDel.status).toBe(200);
+      expect(respDel.body).toBeDefined();
+    }
+    done();
+  });
+*/
+  it("should add and delete new post", async done => {
+    const resp = await request.post("/posts/").send({
+      title: "testForDelete",
+      tags: "#testForDelete",
+      image: "testForDelete",
+      author: "2"
     });
+    const post = await Post.findOne({ title: "testForDelete" });
+    const respDel = await request.delete(`/posts/${post._id}`);
+    expect(respDel.status).toBe(200);
+    expect(respDel.body).toBeDefined();
+    done();
+  });
 
-    it("adding new post", async (done) => {
-        const resp = await request.post("/posts/").send({
-          title: "cat",
-          tags: "#dog",
-          image: "cat",
-          author: "2"
-        });
-    
-        const post = await Post.findOne({ title: "cat" });
-    
-        expect(post.id).toBeTruthy();
-    
-        await Post.findByIdAndDelete(post.id);
-        done();
-      });
-
-    it("getting posts", async (done) => {
-        const resp = await request.get("/posts");
-
-        expect(resp.status).toBe(200);
-        expect(resp.body).toBeDefined();
-        done();
-    });
-
-
-
-    afterAll(async () => {
-        await connection.close();
-        await db.close();
-    });
-
-
-
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
+  });
 });
